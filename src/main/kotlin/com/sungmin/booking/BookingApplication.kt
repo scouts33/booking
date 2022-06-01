8ac15721cf601c979b4076ec407342bd42c2c161
sungmin.lim <scouts33@naver.com>
2022-06-02 01:21:44 +0900

    spring first commit

---
+++ src/main/kotlin/com/sungmin/booking/BookingApplication.kt
@@ -0,0 +1,11 @@
+package com.sungmin.booking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookingApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookingApplication>(*args)
+}
